dc5bea8431dfd2fbe3f764c532b554b03999525d
starudream <justwangsheng@gmail.com>
2023-05-14 21:34:57 +0800

    doc

--- gen_readme.sh
+++ gen_readme.sh
@@ -6,7 +6,6 @@ echo "# sub-rule" >README.md
 for file in $(ls Clash/config | grep .ini); do
   echo "
 - ${file}
-  - https://raw.githubusercontent.com/starudream/sub-rule/master/Clash/config/${file}
   - https://cdn.jsdelivr.net/gh/starudream/sub-rule/Clash/config/${file}
 " >>README.md
 done
