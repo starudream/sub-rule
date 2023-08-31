d483c876164903b656eb9372c19765ad22ee5344
sheng.wang <justwangsheng@gmail.com>
2023-08-31 11:18:34 +0800

    doc

--- gen_readme.sh
+++ gen_readme.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
 echo "# sub-rule" >README.md
+echo "[" >public.json
 
 # shellcheck disable=SC2010
 for file in $(ls Clash/config | grep .ini); do
-  echo "
-- ${file}
-  - https://cdn.jsdelivr.net/gh/starudream/sub-rule/Clash/config/${file}
-" >>README.md
+  echo "- [${file}](https://cdn.jsdelivr.net/gh/starudream/sub-rule/Clash/config/${file})" >>README.md
+  echo >>README.md
+  echo "{\"label\": \"${file}\",\"value\": \"https://cdn.jsdelivr.net/gh/starudream/sub-rule/Clash/config/${file}\"}," >>public.json
 done
+
+sed -i '$ s/.$//' public.json
+echo "]" >>public.json
